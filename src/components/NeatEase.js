e7bfcd4fb722208a97a0248edbad244a11166d33
GustB <2300016092@webmail.uad.ac.id>
2024-12-21 15:53:43 +0700

    ulang

--- src/components/NeatEase.js
+++ src/components/NeatEase.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-const Home = () => {
+const NeatEase = () => {
     return (
         <div className="container flex min-h-screen items-center justify-center mx-auto p4 bg-yellow-200">
             <div className="box flex flex-col items-center justify-center w-96 h-96 bg-yellow-400 rounded-3xl">
@@ -15,4 +15,4 @@ const Home = () => {
     );
 };
 
-export default Home;
+export default NeatEase;
