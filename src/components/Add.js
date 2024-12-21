f9a524368c595d83e292d89e666543a5e66c28db
GustB <2300016092@webmail.uad.ac.id>
2024-12-21 16:09:35 +0700

    ulang

--- src/components/Add.js
+++ src/components/Add.js
@@ -50,7 +50,7 @@ const Add = ({ addTask }) => {
                 />
             </div>
 
-            <Link to="/TaskList">
+            <Link to="/List">
                 <button type="submit" className="bg-yellow-800 text-white px-4 py-2 rounded-2xl w-96 mt-5">
                     Submit
                 </button>
